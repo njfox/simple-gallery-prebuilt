b2ae46fed28438aa7d35b9c0ed070a423d97e72c
Nick Fox <njfox@users.noreply.github.com>
2020-01-03 15:39:01 -0500

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Simple-Gallery
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_SRC_FILES := prebuilt/$(LOCAL_MODULE).apk
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_DEX_PREOPT := false
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
